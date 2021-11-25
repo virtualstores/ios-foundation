289aa38f0c510149b23265248a0736ee777c66a4
cj <carljohan.dahlman@virtualstores.se>
2021-11-25 19:53:07 +0100

    added timestamp values in sensor data class

--- Sources/VSFoundation/Models/SensorFusion/SensorData.swift
+++ Sources/VSFoundation/Models/SensorFusion/SensorData.swift
@@ -9,13 +9,17 @@ import Foundation
 
 
 public class SensorData {
+    
     public let type: SensorDataType
     public let data: [Float]
+    public let timestampSensor: Int
+    public let timestampLocal: Int
     
-    
-    public init(type: SensorDataType, data: [Float]){
+    public init(type: SensorDataType, data: [Float], timestampSensor: Int, timestampLocal: Int){
         self.type = type
         self.data = data
+        self.timestampSensor = timestampSensor
+        self.timestampLocal = timestampLocal
     }
 }
 
