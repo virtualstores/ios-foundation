3fc991f728183501db28ddc057d87270187a32f2
cj <carljohan.dahlman@virtualstores.se>
2021-12-14 18:05:28 +0100

    made Error message class public

--- Sources/VSFoundation/Utilities/ErrorMessage.swift
+++ Sources/VSFoundation/Utilities/ErrorMessage.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-class ErrorMessage {
+public class ErrorMessage {
   static func methodOverride(fileName: String, methodName: String) -> String {
     return "You have to override method: \((fileName as NSString).lastPathComponent):\(methodName)"
   }
