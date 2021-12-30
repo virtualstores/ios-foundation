630a36b913ee71a585c342064ff71a7a775ca34f
Hripsime <hripsime.martirosyan@shortcut.no>
2021-12-30 12:47:45 +0100

    Added PositionBundle

---
+++ Sources/VSFoundation/Models/StepData/PositionBundle.swift
@@ -0,0 +1,12 @@
+//
+// PositionBundle
+// VSEngineWrapper
+//
+// Created by Hripsime on 2021-12-30
+// Copyright Virtual Stores - 2021
+//
+
+public struct PositionBundle {
+    public let position: CGPoint
+    public let std: Double
+}
