21040090edbe3c50b713d1fe4e937b1709f87709
cj <carljohan.dahlman@virtualstores.se>
2022-01-11 10:17:18 +0100

    moving global values and altitude sensor data to foundation

---
+++ Sources/VSFoundation/Extensions/Double+GlobalCollections.swift
@@ -0,0 +1,14 @@
+// Double+GlobalCollections.swift
+// VSSensorInterpreter
+//
+// Created by CJ on 2021-12-09
+// Copyright Virtual Stores - 2021
+//
+
+import Foundation
+
+public extension Array where Element == Double {
+  static let yAxis = [0.0, 1.0, 0.0]
+  static let pocketAxis = [0.0, 0.0, 1.0]
+  static let swingAxis = [0.0, 1.0, 0.0]
+}
---
+++ Sources/VSFoundation/Models/SensorFusion/AltitudeSensorData.swift
@@ -0,0 +1,13 @@
+// AltitudeSensorData.swift
+// VSSensorInterpreter
+
+// Created by: CJ on 2022-01-10
+// Copyright (c) 2022 Virtual Stores
+
+import Foundation
+
+public struct AltitudeSensorData {
+  let data: Double
+  public let timestampSensor: Int
+  public let timestampLocal: Int
+}
