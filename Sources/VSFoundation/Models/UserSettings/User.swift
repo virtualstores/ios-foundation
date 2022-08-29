79a4d74b921f26c718fd811b09890a693186855f
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2022-08-29 16:16:32 +0200

    Changed from userheight to height

--- Sources/VSFoundation/Models/UserSettings/User.swift
+++ Sources/VSFoundation/Models/UserSettings/User.swift
@@ -10,15 +10,15 @@ import Foundation
 final public class User {
     public var id: String?
     public var userId: String?
-    public var userHeight: Float?
+    public var height: Float?
     public var name: String?
     public var age: String?
     public var gender: String?
     
-    public init(id: String?, userId: String?, userHeight: Float?, name: String?, age: String?, gender: String?) {
+    public init(id: String?, userId: String?, height: Float?, name: String?, age: String?, gender: String?) {
         self.id = id
         self.userId = userId
-        self.userHeight = userHeight
+        self.height = height
         self.name = name
         self.age = age
         self.gender = gender
