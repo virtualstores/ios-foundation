770d17ea65a94a4591313db77d6345871cd696d5
Théodore Roos <25298135+TheodoreRoos@users.noreply.github.com>
2024-06-13 10:49:35 +0200

    Added param struct

--- Sources/VSFoundation/VPS/VPSModelManager.swift
+++ Sources/VSFoundation/VPS/VPSModelManager.swift
@@ -9,18 +9,30 @@ import Foundation
 import CoreML
 
 public protocol VPSModelManager {
-  var model: MLModel? { get }
-  var params: VPSModelParams? { get }
+  var mlModel: MLModel? { get }
+  var nlModel: MLModel? { get }
+  var mlParams: VPSMLModelParams? { get }
+  var nlParams: VPSNLModelParams? { get }
 }
 
-public struct VPSModelParams {
+public struct VPSMLModelParams {
   public let frameSize: Int32
   public let useSmooting: Bool
   public let featureSequence: [VPSFeaturesEntriesEnum]
+  public let stepNumberInput: Bool
 
-  public init(frameSize: Int32, useSmooting: Bool, featureSequence: [VPSFeaturesEntriesEnum]) {
+  public init(frameSize: Int32, useSmooting: Bool, featureSequence: [VPSFeaturesEntriesEnum], stepNumberInput: Bool) {
     self.frameSize = frameSize
     self.useSmooting = useSmooting
     self.featureSequence = featureSequence
+    self.stepNumberInput = stepNumberInput
+  }
+}
+
+public struct VPSNLModelParams {
+  public let resolution: Double
+
+  public init(resolution: Double) {
+    self.resolution = resolution
   }
 }
